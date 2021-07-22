8cf70da2e5b340e4b5d6e991eb2c8cf6b3e6075d
felipe cruz <felipephplow@gmail.com>
2021-07-22 16:52:01 -0300

    dasda

--- resources/views/store/pages/user/pedido-produtos.blade.php
+++ resources/views/store/pages/user/pedido-produtos.blade.php
@@ -22,64 +22,74 @@
     </div>
     <div class="status">
         @foreach ($orders as $order)
-            <div class="pedidos-single my-4">
+            <div class="pedidos-single my-4 text-uppercase">
+
                 <div class="pedidos-body row justify-content-between">
-                    <div class="col-3">
-                        <h4>PRODUTO</h4>
+                    <div class="col-6">
+                        <h4>PRODUTO: {{ $order->products->name }}</h4>
                     </div>
-                    <div class="col-3">
-                        <h4>QUANT.</h4>
-                    </div>
-                    <div class="col-3">
-                        <h4>VALOR</h4>
-                    </div>
-                    <div class="col-3">
-                        <h4>PESCADOR</h4>
+                    <div class="col-6">
+                        <h4>QUANTIDADE: {{ $order->products->quantity }} KG</h4>
                     </div>
                 </div>
                 <div class="pedidos-body row justify-content-between">
-                    <div class="col-3">
-                        <h4>{{ $order->products->name }}</h4>
+                    <div class="col-6">
+                        <h4>Preço: {{ '€ ' . number_format($order->products->price, 2, ',', '.') }} </h4>
                     </div>
-                    <div class="col-3">
-                        <h4>{{ $order->products->quantity }} KG</h4>
-                    </div>
-                    <div class="col-3">
-                        <h4>{{ '€ ' . number_format($order->products->price, 2, ',', '.') }} </h4>
-                    </div>
-                    <div class="col-3">
-                        <h4>{{ $order->pescador->name }}</h4>
+                    <div class="col-6">
+                        <h4>PESCADOR: {{ $order->pescador->name }}</h4>
                     </div>
                 </div>
+                <div class="pedidos-body row justify-content-between">
+                    <div class="col-12">
+                        <h4>Status:  @if ($order->products->status == 0) AGUARDANDO PAGAMENTO
+                            @elseif($order->products->status == 1) ANÁLISE FINANCEIRA
+                            @elseif($order->products->status == 2) PAGAMENTO ACEITO
+                            @elseif($order->products->status == 3) A LIBERAR
+                            @elseif($order->products->status == 4) EM TRANSPORTE
+                            @elseif($order->products->status == 5) ENTREGUE
+                            @elseif($order->products->status == 6) CANCELADO @endif</h4>
+                    </div>
+
+                </div>
+                <div class="pedidos-body row justify-content-between">
+                    <div class="col-12 text-center">
+                        <button type="button" class="btn btn-primary">INFORMAR RECEBIMENTO</button>
+                    </div>
+
+
+                </div>
+
+
             </div>
         @endforeach
     </div>
     <div class="status">
 
-            <div class="pedidos-single my-4">
-                <div class="pedidos-body row justify-content-between">
-                    <div class="col-4">
-                        <h4>FRETE</h4>
-                    </div>
-                    <div class="col-4">
-                        <h4>SUBTOTAL.</h4>
-                    </div>
-                    <div class="col-4">
-                        <h4>TOTAL</h4>
-                    </div>
+        <div class="pedidos-single my-4">
+            <div class="pedidos-body row justify-content-between">
+                <div class="col-4">
+                    <h4>FRETE</h4>
                 </div>
-                <div class="pedidos-body row justify-content-between">
-                    <div class="col-4">
-                        <h4>{{ '€ ' . number_format($user_order->frete, 2, ',', '.') }}</h4>
-                    </div>
-                    <div class="col-4">
-                        <h4>{{ '€ ' . number_format($user_order->sub_total, 2, ',', '.') }}</h4>
-                    </div>
-                    <div class="col-4">
-                        <h4>{{ '€ ' . number_format($user_order->total, 2, ',', '.') }}</h4>
-                    </div>
+                <div class="col-4">
+                    <h4>SUBTOTAL.</h4>
+                </div>
+                <div class="col-4">
+                    <h4>TOTAL</h4>
                 </div>
             </div>
+            <div class="pedidos-body row justify-content-between">
+                <div class="col-4">
+                    <h4>{{ '€ ' . number_format($user_order->frete, 2, ',', '.') }}</h4>
+                </div>
+                <div class="col-4">
+                    <h4>{{ '€ ' . number_format($user_order->sub_total, 2, ',', '.') }}</h4>
+                </div>
+                <div class="col-4">
+                    <h4>{{ '€ ' . number_format($user_order->total, 2, ',', '.') }}</h4>
+                </div>
+            </div>
+        </div>
 
     </div>
     <div class="square">
@@ -118,10 +128,10 @@
                 <span>Endereço</span>
                 <div>
                     <span>
-                       Morada: {{ $user_order->enderecos->morada }}, {{ $user_order->enderecos->porta }}<br>
-                       Código Postal: {{ $user_order->enderecos->codigo_postal }}<br>
-                       Distrito: {{ $user_order->enderecos->distrito }}<br>
-                       Freguesia: {{ $user_order->enderecos->freguesia }}<br>
+                        Morada: {{ $user_order->enderecos->morada }}, {{ $user_order->enderecos->porta }}<br>
+                        Código Postal: {{ $user_order->enderecos->codigo_postal }}<br>
+                        Distrito: {{ $user_order->enderecos->distrito }}<br>
+                        Freguesia: {{ $user_order->enderecos->freguesia }}<br>
 
                     </span>
                 </div>
