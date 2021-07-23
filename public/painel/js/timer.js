72f03f856d8980f90ed794863f71e2db62022585
felipe cruz <felipephplow@gmail.com>
2021-07-23 16:15:08 -0300

    da

--- public/painel/js/timer.js
+++ public/painel/js/timer.js
@@ -4,7 +4,6 @@ $('[data-countdown]').each(function () {
       var $this = $(this), finalDate = $(this).data('countdown');
       $this.countdown(finalDate, function (event) {
             $this.html(event.strftime('%H:%M:%S'));
-            console.log($this2);
           if(event.offset.hours == 0 && event.offset.minutes == 0 && event.offset.seconds == 0){
             $this2.parent().parent().remove();
           }
