feb5503d4f457fd7ca695075be8aa0b46c1fad21
felipe cruz <felipephplow@gmail.com>
2021-08-09 15:08:05 -0300

    ds

--- resources/views/store/pages/painel/thanks.blade.php
+++ resources/views/store/pages/painel/thanks.blade.php
@@ -18,6 +18,13 @@
       <div class="texto-ok text-center">
             <h1>PEDIDO REALIZADO</h1>
       </div>
+      <div class="texto-ok text-center text-uppercase">
+            <h5>Conta para Transferência</h5>
+            <div>
+                <span>banco Montepio - numero de conta : 295.10.005582-7 <br> BIC/SWIFT : MPIOPTPL <br> NIB :
+                    0036.0295.99100055827.07 <br> IBAN: PT50.0036.0295.99100055827.07</span>
+            </div>
+      </div>
       <div class="text-center mt-3">
            <a href="{{ route('user.pedidos') }}"> <button class="botao-enc">Ver encomendas</button></a>
       </div>
