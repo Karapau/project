0c0c7b9dbce7c97d81c06a07f3d0dcdfac4e7c29
felipe cruz <felipephplow@gmail.com>
2021-08-10 10:03:53 -0300

    d

--- resources/views/app-front/store/pages/checkout.blade.php
+++ resources/views/app-front/store/pages/checkout.blade.php
@@ -184,7 +184,7 @@
             </div>
         </div>
     </div> --}}
-        <div class="top_4">
+        {{-- <div class="top_4">
             <div class="container">
                 <div class="metodos">
                     <div class="logo-metodos">
@@ -198,7 +198,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
         <div id="phone" class="d-none">
             <div class="container row justify-content-center my-3">
                 <div class="col-10">
