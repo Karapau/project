fd5e7bbfd640f387215f5064b3ba47cae9a17d86
felipe cruz <felipephplow@gmail.com>
2021-08-11 18:22:33 -0300

    s

--- resources/views/app-front/store/pages/produtos.blade.php
+++ resources/views/app-front/store/pages/produtos.blade.php
@@ -9,7 +9,7 @@
     <div class="botao-v">
         <a class="btn btn-voltar" href="/store-porto">TROCAR</a>
     </div>
-    <div class="container">
+    <div class="container" >
         <div class="filtrar_1 mt-4">
             <div>
                 <button class="btn btn-filtro" data-bs-toggle="modal" data-bs-target="#exampleModal">FILTRAR</button>
@@ -68,50 +68,51 @@
 
             </div>
         </div>
-        @component('components.btn-comprar.btn-compra')
+    </div>
+    @component('components.btn-comprar.btn-compra')
 
-        @endcomponent
+    @endcomponent
 
-        @component('components.modal.filtro-modal', ['title' => 'Buscar Produto'])
-            <form action="{{ route('store.produto.buscar') }}" method="get">
-                @csrf
-                <div class="mb-3">
-                    <select name="especie_id" class="form-control" id="">
-                        <option value="">Espécie</option>
-                        @foreach ($especies as $especie)
-                            <option value="{{ $especie->id }}">{{ $especie->nome_portugues }}</option>
-                        @endforeach
+    @component('components.modal.filtro-modal', ['title' => 'Buscar Produto'])
+        <form action="{{ route('store.produto.buscar') }}" method="get">
+            @csrf
+            <div class="mb-3">
+                <select name="especie_id" class="form-control" id="">
+                    <option value="">Espécie</option>
+                    @foreach ($especies as $especie)
+                        <option value="{{ $especie->id }}">{{ $especie->nome_portugues }}</option>
+                    @endforeach
 
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <select name="tamanho" class="form-control" id="">
-                        <option value="">Tamanho</option>
-                        <option value="tamanho1">Tamanho 1 (T1)</option>
-                        <option value="tamanho2">Tamanho 2 (T2)</option>
-                        <option value="tamanho3">Tamanho 3 (T3)</option>
-                        <option value="tamanho4">Tamanho 4 (T4)</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <select name="arte" class="form-control" id="">
-                        <option value="">Arte</option>
-                        <option value="rede">Rede</option>
-                        <option value="vara">Vara</option>
-                        <option value="cerco">Cerco</option>
-                        <option value="arrasto">Arrasto</option>
-                        <option value="redes_de_emalhar">Redes de emalhar</option>
-                        <option value="redes_de_tresmalho">Redes de Tresmalho</option>
-                        <option value="anzol">Anzol</option>
-                        <option value="armadilhas">Armadilhas</option>
-                        <option value="envolventes_arrastantes">Envolventes arrastantes</option>
-                        <option value="arte_xavega">Arte Xávega</option>
-                        <option value="apanha">Apanha</option>
-                    </select>
-                </div>
-                <div class="text-center">
-                    <button type="submit" class="btn btn-info">Buscar</button>
-                </div>
-            </form>
-        @endcomponent
-    @endsection
+                </select>
+            </div>
+            <div class="mb-3">
+                <select name="tamanho" class="form-control" id="">
+                    <option value="">Tamanho</option>
+                    <option value="tamanho1">Tamanho 1 (T1)</option>
+                    <option value="tamanho2">Tamanho 2 (T2)</option>
+                    <option value="tamanho3">Tamanho 3 (T3)</option>
+                    <option value="tamanho4">Tamanho 4 (T4)</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <select name="arte" class="form-control" id="">
+                    <option value="">Arte</option>
+                    <option value="rede">Rede</option>
+                    <option value="vara">Vara</option>
+                    <option value="cerco">Cerco</option>
+                    <option value="arrasto">Arrasto</option>
+                    <option value="redes_de_emalhar">Redes de emalhar</option>
+                    <option value="redes_de_tresmalho">Redes de Tresmalho</option>
+                    <option value="anzol">Anzol</option>
+                    <option value="armadilhas">Armadilhas</option>
+                    <option value="envolventes_arrastantes">Envolventes arrastantes</option>
+                    <option value="arte_xavega">Arte Xávega</option>
+                    <option value="apanha">Apanha</option>
+                </select>
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-info">Buscar</button>
+            </div>
+        </form>
+    @endcomponent
+@endsection
