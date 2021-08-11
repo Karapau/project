6630ebb6a3f7ecc86ce9d4ba699d248528bbfcdd
felipe cruz <felipephplow@gmail.com>
2021-08-11 18:14:23 -0300

    ds

--- resources/views/painel/pages/encomendas/pedido.blade.php
+++ resources/views/painel/pages/encomendas/pedido.blade.php
@@ -123,7 +123,7 @@
                                                         <tbody>
                                                             <tr class="table-light">
                                                                 <td><button type="button" class="btn btn-dark btn-sm">{{$userProduct->origem}}</button></td>
-                                                                <td><button type="button" class="btn {{$userProduct->status == 0 ? 'btn-dark' : 'btn-success'}} btn-sm @if($userProduct->status == 1) btn_liberar_pedido @endif"  data-route="{{route('admin.status.produto')}}" data-id="{{$userProduct->id}}">{{$userProduct->status == 1 ? 'A LIBERAR' : 'LIBERADO'}}</button></td>
+                                                                <td><button type="button" class="btn {{$userProduct->status == 0 ? 'btn-dark' : 'btn-success'}} btn-sm @if($userProduct->status == 1) btn_liberar_pedido @endif"  data-route="{{route('admin.status.produto')}}" data-id="{{$userProduct->id}}">{{$userProduct->status == 1 ? 'A LIBERAR' : 'LIBERARAR'}}</button></td>
                                                                 <td><button type="button" class="btn btn-dark btn-sm">ENVIAR</button></td>
                                                                 <td><button type="button" class="btn btn-dark btn-sm">GERAR</button></td>
                                                                 <td><button type="button" class="btn btn-dark btn-sm">ESCOLHER ENTREGADOR</button></td>
